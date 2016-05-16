accf13c7e3f77bc8f3a1765355e71779f36b429c
unknown <>
2016-05-16 21:39:54 +0800

    開始畫面貓的鬍鬚動畫

--- src/MainApplet.java
+++ src/MainApplet.java
@@ -13,10 +13,10 @@ import processing.data.JSONObject;
 public class MainApplet extends PApplet
 {
 	private final static int width = 960, height = 540;
-	private PImage middleroom, rightroom, leftroom, start;
+	private PImage middleroom, rightroom, leftroom, start, start2;
 	private int curRoom;
 	private ControlP5 cp5;
-	
+	private int catMove = 0;
 	public void setup()				// override the processing that initial the applet
 	{	
 		Ani.init(this);
@@ -25,6 +25,7 @@ public class MainApplet extends PApplet
 		middleroom = loadImage("background/middle.png");
 		rightroom = loadImage("background/right.png");
 		start = loadImage("background/start.jpg");
+		start2 = loadImage("background/start2.jpg");
 		curRoom = 2;
 		
 		
@@ -47,8 +48,16 @@ public class MainApplet extends PApplet
 			image(rightroom, 0, 0, 840, 540);
 		else if(this.curRoom == -1)
 			image(leftroom, 0, 0, 840, 540);
-		else if(this.curRoom == 2)
-			image(start, 0, 0, 960, 540);
+		else if(this.curRoom == 2){
+			if(this.catMove < 10 )
+				image(start, 0, 0, 960, 540);
+			else
+				image(start2, 0, 0, 960, 540);
+		}
+		if(this.catMove < 19)
+			this.catMove ++;
+		else
+			this.catMove = 0;
 	}
 	
 	public void keyPressed(KeyEvent arg0)
