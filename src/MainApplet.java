0ce94fb928c8adb0e85eca1101d886cd463b35e7
jasonchang117 <jasonchang117@gmail.com>
2016-05-16 20:41:06 +0800

    background transfer via arrow

--- src/MainApplet.java
+++ src/MainApplet.java
@@ -14,6 +14,7 @@ public class MainApplet extends PApplet
 {
 	private final static int width = 960, height = 540;
 	private PImage middleroom, rightroom, leftroom, start;
+	private int curRoom;
 	
 	public void setup()				// override the processing that initial the applet
 	{	
@@ -22,6 +23,7 @@ public class MainApplet extends PApplet
 		leftroom = loadImage("background/left.png");
 		middleroom = loadImage("background/middle.png");
 		rightroom = loadImage("background/right.png");
+		curRoom = 0;
 		
 		size(width, height);
 	}
@@ -29,6 +31,23 @@ public class MainApplet extends PApplet
 	public void draw()				// override the processing that paint the main components
 	{
 		background(0);
-		image(middleroom, 0, 0, 840, 540);
+		if(this.curRoom == 0)
+			image(middleroom, 0, 0, 840, 540);
+		else if(this.curRoom == 1)
+			image(rightroom, 0, 0, 840, 540);
+		else if(this.curRoom == -1)
+			image(leftroom, 0, 0, 840, 540);
+	}
+	
+	public void keyPressed(KeyEvent arg0)
+	{
+		if(arg0.getKeyCode() == KeyEvent.VK_RIGHT && curRoom < 1)
+		{
+			this.curRoom += 1;
+		}
+		else if(arg0.getKeyCode() == KeyEvent.VK_LEFT && curRoom > -1)
+		{
+			this.curRoom -= 1;
+		}
 	}
 }
