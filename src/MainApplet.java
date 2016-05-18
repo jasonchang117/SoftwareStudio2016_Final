5bf434515ca3000b7ae7445ed6cf33ec281adbf8
jasonchang117 <jasonchang117@gmail.com>
2016-05-18 14:55:37 +0800

    fix bugs of backbutton and buttons didn't show after return

--- src/MainApplet.java
+++ src/MainApplet.java
@@ -21,7 +21,7 @@ public class MainApplet extends PApplet
 	private PImage middleroom, rightroom, leftroom, start, start2, passwordBackground;
 	private int curRoom;
 	private ControlP5 cp5;
-	private int catMove = 0, question = 0;
+	private int catMove = 0, question = 0, backbutton = 0, startmenu = 1;
 	private  AudioClip cat;
 	private JTextField password = new JTextField();
 	private LeftRoom leftRoom = new LeftRoom(this);
@@ -45,9 +45,9 @@ public class MainApplet extends PApplet
 		cp5 =  new ControlP5(this);
 		PFont p = createFont("Consolas", 20);
 		cp5.setFont(p);
-		cp5.addButton("buttonA").setLabel("Voice On").setPosition(60, 450).setSize(200, 50);
-		cp5.addButton("buttonB").setLabel("Start").setPosition(380, 450).setSize(200, 50);
-		cp5.addButton("buttonC").setLabel("questionnaire").setPosition(700, 450).setSize(200,50);
+		cp5.addButton("voice").setLabel("Voice On").setPosition(60, 450).setSize(200, 50);
+		cp5.addButton("startbutton").setLabel("Start").setPosition(380, 450).setSize(200, 50);
+		cp5.addButton("questionnaire").setLabel("questionnaire").setPosition(700, 450).setSize(200,50);
 		
 		// password
 		password.setBounds(0,430,300,30);
@@ -62,24 +62,37 @@ public class MainApplet extends PApplet
 		
 		if(this.curRoom == 0)
 		{
-			
 			if(question == 1)
 				image(passwordBackground, 0, 0, 960, 540);
 			else
 				image(middleroom, 0, 0, 840, 540);
-			cp5.remove("buttonA");
-			cp5.remove("buttonB");
-			cp5.remove("buttonC");
 			
+			if(this.startmenu == 1)
+			{
+				cp5.remove("voice");
+				cp5.remove("startbutton");
+				cp5.remove("questionnaire");
+				this.startmenu = 0;
+			}
 		}
 		else if(this.curRoom == 1)
+		{
 			image(rightroom, 0, 0, 840, 540);
-		else if(this.curRoom == -1){
+		}
+		else if(this.curRoom == -1)
+		{
 			image(leftroom, 0, 0, 840, 540);
 			leftRoom.drawknif();
 		}
 		else if(this.curRoom == 2)
 		{
+			if(this.startmenu == 0)
+			{
+				cp5.addButton("voice").setLabel("Voice On").setPosition(60, 450).setSize(200, 50);
+				cp5.addButton("startbutton").setLabel("Start").setPosition(380, 450).setSize(200, 50);
+				cp5.addButton("questionnaire").setLabel("questionnaire").setPosition(700, 450).setSize(200,50);
+				this.startmenu = 1;
+			}
 			if(this.catMove < 10 )
 				image(start, 0, 0, 960, 540);
 			else
@@ -91,11 +104,16 @@ public class MainApplet extends PApplet
 		else
 			this.catMove = 0;
 		
-		if( (this.curRoom ==0 || this.curRoom ==1 || this.curRoom == -1) )     // waiting for modifying
+		if( (this.curRoom == 0 || this.curRoom == 1 || this.curRoom == -1) && backbutton == 0)     // waiting for modifying
+		{
 			cp5.addButton("buttonBack").setLabel("Back").setPosition(860, 480).setSize(50,50);
-		else
+			this.backbutton = 1;
+		}
+		else if(this.curRoom == 2 && backbutton == 1)
+		{
 			cp5.remove("buttonBack");
-		
+			this.backbutton = 0;
+		}
 	}
 	
 	public void keyPressed(KeyEvent arg0)
@@ -110,17 +128,17 @@ public class MainApplet extends PApplet
 		}
 	}
 	
-	public void buttonA()
+	public void voice()
 	{
 		
 	}
 	
-	public void buttonB()
+	public void startbutton()
 	{
 		this.curRoom = 0;
 	}
 	
-	public void buttonC()
+	public void questionnaire()
 	{
 		this.curRoom = 0;
 		this.question = 1;
