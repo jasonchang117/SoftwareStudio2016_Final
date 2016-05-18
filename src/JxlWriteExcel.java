49e4e77afe608056e7b27f00ac6b399ab1f18541
顏均穎 <cde1234g@gmail.com>
2016-05-18 23:32:49 +0800

    Modify JxlWriteExcel.java and a little main(). Waiting for input text to load in excel.

--- src/JxlWriteExcel.java
+++ src/JxlWriteExcel.java
@@ -14,14 +14,14 @@ import jxl.format.Alignment;
 import jxl.format.CellFormat;
 import jxl.format.Colour;
 
-public class JxlWriteExcel {
+public interface JxlWriteExcel {
 
-	public JxlWriteExcel() {
+	public static void JxlWriteExcel(String text) {
 		try{
 			WritableWorkbook workbook = Workbook.createWorkbook(new File("output.xls"));
 			WritableSheet sheet = workbook.createSheet("First Sheet", 0);
 			 
-			/*
+			
 			WritableFont myFont = new WritableFont(WritableFont.createFont("標楷體"), 14);
 			myFont.setColour(Colour.WHITE);
 			WritableCellFormat cellFormat = new WritableCellFormat();
@@ -29,9 +29,9 @@ public class JxlWriteExcel {
 			cellFormat.setFont(myFont); // 指定字型
 			cellFormat.setBackground(Colour.LIGHT_BLUE); // 背景顏色
 			cellFormat.setAlignment(Alignment.CENTRE); // 對齊方式
-			*/
 			
-			//System set the format type: Date - Question - Option1 - Option2 - Option3. 
+			
+			//System set the format type: Date - Question - Yes - No. 
 			Label typeD = new Label(0, 0, "Date"); 
 			sheet.addCell(typeD); 
 			Label typeQ = new Label(0, 0, "Question"); 
@@ -40,8 +40,6 @@ public class JxlWriteExcel {
 			sheet.addCell(typeO1);
 			Label typeO2 = new Label(0, 0, "Option2"); 
 			sheet.addCell(typeO2);
-			Label typeO3 = new Label(0, 0, "Option3"); 
-			sheet.addCell(typeO3);
 			
 			  // Get the current date and time from the Calendar object .
 			Date now = Calendar.getInstance().getTime(); 
@@ -49,26 +47,13 @@ public class JxlWriteExcel {
 			WritableCellFormat dateFormat = new WritableCellFormat (customDateFormat); 
 			DateTime dateCell = new DateTime(0, 1, now, dateFormat); 
 			sheet.addCell(dateCell); 
-			
+		/*	
 			// Get the question for the questionnaire.
 			System.out.print("Please enter the question for the questionnaire:");
 			Scanner input = new Scanner(System.in);
-			String s = new String(input.nextLine());
-			Label label = new Label(1, 1, s); 
+			String s = new String(input.nextLine());*/
+			Label label = new Label(1, 1, text); 
 			sheet.addCell(label); 
-			
-			//Get the options for the questionnaire.
-			System.out.println("Please enter the options for your question.");
-			System.out.println("Option1:");
-			Scanner input1 = new Scanner(System.in);
-			String o1 = new String(input1.nextLine());
-			Label label1 = new Label(1, 1, o1); 
-			sheet.addCell(label1); 
-			System.out.println("Option2:");
-			Scanner input2 = new Scanner(System.in);
-			String o2 = new String(input2.nextLine());
-			Label label2 = new Label(1, 1, o2); 
-			sheet.addCell(label2); 
 			
 			workbook.write(); 
 			workbook.close();
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import javax.swing.JFrame;
 
+import test_charbar.TextQ;
+
 @SuppressWarnings("serial")
-public class Main extends JFrame{
+public class Main extends JFrame implements JxlWriteExcel {
 	
 	private final static int windowWidth = 960, windowHeight = 540;
+	private static String question = new String("12345678");
 	
 	public static void main(String [] args)
 	{
@@ -12,10 +15,16 @@ public class Main extends JFrame{
 		applet.start();
 		applet.setFocusable(true);
 		
-		JFrame window = new JFrame("Star Wars Network Analysis");
+		JFrame window = new JFrame("Pusheen-Escape the Room & Made Questionnaire.");
 		window.setContentPane(applet);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(windowWidth, windowHeight);
 		window.setVisible(true);
+		
+		/**Waiting for Turtle's class.**/
+		Text textq = new TextQ();
+		question = textq.getText();
+		JxlWriteExcel(question);
 	}
+
 }
