9eb902aeee66d13b246783e7c2dd9af2db3ad349
Emily <emily051485@gmail.com>
2016-05-19 21:57:43 +0800

    leftroom

--- src/LeftRoom.java
+++ src/LeftRoom.java
@@ -24,6 +24,29 @@ public class LeftRoom
 		
 	}
 	
+
+	public int hammer() { return this.hammer; }
+	public int pusheenBack() { return this.pusheenBack; }
+	public int pusheenFront() { return this.pusheenFront; }
+	public int pusheenLeft() { return this.pusheenLeft; }
+	public int pusheenCut() { return this.pusheenCut; }
+	public int pusheenWithoutHammer() { return this.pusheenWithoutHammer; }
+	public int pusheenBeKnif() { return this.pusheenBeKnif; }
 	
+	public void hammer_vanish() { this.hammer = 0; }
+	public void pusheenBack_vanish() {this.pusheenBack = 0; }
+	public void pusheenFront_vanish() { this.pusheenFront = 0; }
+	public void pusheenLeft_vanish() { this.pusheenLeft = 0; }
+	public void pusheenCut_vanish() { this.pusheenCut = 0; }
+	public void pusheenWithoutHammer_vanish() { this.pusheenWithoutHammer = 0; }
+	public void pusheenBeKnif_vanish() { this.pusheenBeKnif = 0; }
+	
+	public void hammer_appear() { this.hammer = 1; }
+	public void pusheenBack_appear() {this.pusheenBack = 1; }
+	public void pusheenFront_appear() { this.pusheenFront = 1; }
+	public void pusheenLeft_appear() { this.pusheenLeft = 1; }
+	public void pusheenCut_appear() { this.pusheenCut = 1; }
+	public void pusheenWithoutHammer_appear() { this.pusheenWithoutHammer = 1; }
+	public void pusheenBeKnif_appear() { this.pusheenBeKnif = 1; }
 	
 }
