c7b0ae3c7c0cd471e864ce8434a21fa98de8fd06
Emily <emily051485@gmail.com>
2016-05-26 15:47:20 +0800

    剛剛忘記push~~

--- src/Itemtable.java
+++ src/Itemtable.java
@@ -3,20 +3,26 @@ public class Itemtable {
 	private int tape;		// 0->not visible, 1->visible
 	private int knif;						// 0->not visible, 1->visible
 	private int pusheenBottle;
+	private int normalBottleFull;
 	private int lighter;	
 	private int hammer;
 	private int hose;
 	private int normalBottle;
+	private int pusheenBottleFull;
+	private int normalBottleWithHose;
 	
 	public Itemtable() {
 		// TODO Auto-generated constructor stub
 		tape = 1;
 		knif = 1;
 		pusheenBottle = 1;
+		normalBottleFull = 1;
 		lighter = 1;
 		hammer = 1;
 		hose = 1;
 		normalBottle = 1;
+		pusheenBottleFull = 1;
+		normalBottleWithHose = 1;
 	}
 
 	
@@ -29,6 +35,9 @@ public class Itemtable {
 	public int hammer() { return this.hammer; }
 	public int hose(){return this.hose; }
 	public int normalBottle(){return this.normalBottle; }
+	public int pusheenBottleFull() {return this.pusheenBottleFull;}
+	public int normalBottleWithHose() {return this.normalBottleWithHose;}
+	public int normalBottleFull() {return this.normalBottleFull;}
 	
 	
 	public void tape_vanish() { this.tape = 0; }
@@ -38,6 +47,9 @@ public class Itemtable {
 	public void hammer_vanish() { this.hammer = 0; }
 	public void hose_vanish(){	this.hose = 0;}
 	public void normalBottle_vanish(){	this.normalBottle = 0;}
+	public void pusheenBottleFull_vanish() { this.pusheenBottleFull = 0;}
+	public void normalBottleFull_vanish() {this.normalBottleFull = 0;}
+	public void normalBottleFullWithHose_vanish() {this.normalBottleWithHose = 0;}
 	
 	public void tape_appear() { this.tape = 1; }
 	public void knif_appear() { this.knif = 1; }
@@ -46,5 +58,8 @@ public class Itemtable {
 	public void hammer_appear() { this.hammer = 1; }
 	public void hose_appear(){this.hose = 1;}
 	public void normalBottle_appear(){this.normalBottle = 1;}
+	public void pusheenBottleFull_appear() {this.pusheenBottleFull = 1;}
+	public void normalBottleFull_appear() {this.normalBottleFull = 1;}
+	public void normalBottleFullWithHose_appear() {this.normalBottleWithHose = 1;}
 	
 }
